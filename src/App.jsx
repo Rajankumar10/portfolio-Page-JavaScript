689e8669336be0beefe1768f180072c990c7b178
RAJAN KUMAR <rajankumar10022002@gmail.com>
2025-02-14 13:27:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+
+import "./styles.css";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Rajan Kumar",
+  title: "Web Designer",
+  email: "rajankumar10022002@gmail.com",
+  gitHub: "https://github.com/Rajankumar10",
+  linkedIn: "https://www.linkedin.com/in/rajan-kumar-8a754b272/",
+  medium: "",
+  leetcode: "https://leetcode.com/u/Rajan_kumar10022002/",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
+    </div>
+  );
+};
+
+export default App;
